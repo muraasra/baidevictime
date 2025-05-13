b9f41f04ea7fa40c0e3ebc31308532b7bd15a77b
muraasra <wilfriedtayou6@gmail.com>
2025-05-13 09:43:47 +0200

    en mode bd sqlLite

--- aide_victime/settings.py
+++ aide_victime/settings.py
@@ -130,14 +130,20 @@ WSGI_APPLICATION = 'aide_victime.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cipcre_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cipcre_db',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
